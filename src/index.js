5aa9db4059d767a4fd324ca356bfd8b3aa1f7212
MarioGeorgiev <37220117+MarioGeorgiev@users.noreply.github.com>
2023-02-14 07:27:05 +0200

    Mario now can jump more then once

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const mario = document.getElementById('mario')
 console.log(mario)
-function jump(){
-   
+function jump(){   
     mario.classList.add('jump')
+    setTimeout(function(){
+        mario.classList.remove('jump')
+    },300)
 }
 
 document.addEventListener("keypress", (ev)=>{
