58e7c1d3eb4128dc2c1db92d4cfec6596b2ccfd1
MarioGeorgiev <37220117+MarioGeorgiev@users.noreply.github.com>
2023-02-14 07:30:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const mario = document.getElementById('mario')
 console.log(mario)
 function jump(){   
+    if(!mario.classList.contains("jump")){
     mario.classList.add('jump')
     setTimeout(function(){
         mario.classList.remove('jump')
-    },300)
+    },300)}
 }
 
 document.addEventListener("keypress", (ev)=>{
